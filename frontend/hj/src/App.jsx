9b9d0e510b0e46c8dcf45b565e6da502cd06ce06
Sdg877 <sdrakegill@gmail.com>
2025-01-24 10:45:15 +0000

    Route for news update

--- frontend/hj/src/App.jsx
+++ frontend/hj/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import UploadImages from "./admin/ImageUpload";
+import NewsUpdate from './admin/NewsUpdate';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/images" element={<UploadImages />} />
+          <Route path="/news" element={<NewsUpdate />} />
         </Routes>
       </div>
     </Router>
