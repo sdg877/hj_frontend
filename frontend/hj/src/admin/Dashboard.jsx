fc7ca0f6d8b5814244418c010683068d8ebcdb55
Sdg877 <sdrakegill@gmail.com>
2025-01-25 07:16:01 +0000

    Dashboard set up

--- frontend/hj/src/admin/Dashboard.jsx
+++ frontend/hj/src/admin/Dashboard.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import ImageUpload from './forms/ImageUpload'
+import NewsUpdate from './forms/NewsUpdate'
 
 const Dashboard = () => {
   return (
+    <>
     <div>
-      
+      <h2>Hi Heather. Welcome To Your Dashboard.</h2>
+      <ImageUpload />
+      <NewsUpdate />
     </div>
+    </>
   )
 }
 
