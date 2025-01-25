390636ab4aa4730ce5385fe4b87747671b76853d
Sdg877 <sdrakegill@gmail.com>
2025-01-25 06:48:36 +0000

    Frontend routes added

--- frontend/hj/src/App.jsx
+++ frontend/hj/src/App.jsx
@@ -1,17 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Dashboard from './admin/Dashboard.jsx'
 import UploadImages from "./admin/forms/ImageUpload";
-import NewsUpdate from './admin/forms/NewsUpdate';
+import NewsUpdate from "./admin/forms/NewsUpdate";
+import AboutMe from "./site/pages/AboutMe.jsx";
+import ContactMe from "./site/pages/ContactMe.jsx";
+import Gallery from "./site/pages/Gallery.jsx";
+import Homepage from "./site/pages/Homepage.jsx";
+import News from "./site/pages/News.jsx";
+import Login from "./admin/forms/Login.jsx";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/images" element={<UploadImages />} />
-          <Route path="/news" element={<NewsUpdate />} />
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/admin" element={<Dashboard />} />
+          <Route path="/admin/images" element={<UploadImages />} />
+          <Route path="/admin/news" element={<NewsUpdate />} />
+
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/contact" element={<ContactMe />} />
+          <Route path="/" element={<Homepage />} />
         </Routes>
       </div>
     </Router>
