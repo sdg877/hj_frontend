4fd2efc45229db148a5e7b13ef8cb236433ff7c8
Sdg877 <sdrakegill@gmail.com>
2025-01-26 07:13:26 +0000

    Navbar implemented

--- frontend/hj/src/App.jsx
+++ frontend/hj/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Dashboard from './admin/Dashboard.jsx';
+import Dashboard from "./admin/Dashboard.jsx";
 import UploadImages from "./admin/forms/ImageUpload";
 import NewsUpdate from "./admin/forms/NewsUpdate";
 import AboutMe from "./site/pages/AboutMe.jsx";
@@ -10,18 +10,41 @@ import Gallery from "./site/pages/Gallery.jsx";
 import Homepage from "./site/pages/Homepage.jsx";
 import News from "./site/pages/News.jsx";
 import Login from "./admin/forms/Login.jsx";
-import PrivateRoute from './PrivateRoute';  
+import PrivateRoute from "./PrivateRoute";
+import Navbar from "../src/site/components/Navbar.jsx";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar /> 
         <Routes>
           <Route path="/login" element={<Login />} />
-          
-          <Route path="/admin" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-          <Route path="/admin/images" element={<PrivateRoute><UploadImages /></PrivateRoute>} />
-          <Route path="/admin/news" element={<PrivateRoute><NewsUpdate /></PrivateRoute>} />
+
+          <Route
+            path="/admin"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/admin/images"
+            element={
+              <PrivateRoute>
+                <UploadImages />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/admin/news"
+            element={
+              <PrivateRoute>
+                <NewsUpdate />
+              </PrivateRoute>
+            }
+          />
 
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/about" element={<AboutMe />} />
