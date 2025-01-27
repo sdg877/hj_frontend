8fe8ca9cea97c31f13b0c4e67d2e75bb40c14c7d
Sdg877 <sdrakegill@gmail.com>
2025-01-27 08:11:27 +0000

    Paintings gallery implemented

---
+++ frontend/hj/src/site/components/paintings.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import '../../App.css';
+
+const Paintings = () => {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+
+        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/gallery/paintings`);
+        setImages(response.data.images || []);
+        setLoading(false);
+      } catch (err) {
+        console.error("Error fetching images:", err);
+        setError("Error fetching images.");
+        setLoading(false);
+      }
+    };
+
+    fetchImages();
+  }, []);
+
+  return (
+    <div>
+      <h1>Paintings</h1>
+      {loading && <p>Loading images...</p>}
+      {error && <p>{error}</p>}
+      <div className="image-gallery">
+        {images.length > 0 ? (
+          images.map((image, index) => (
+            <div key={index} className="image-item">
+              <img src={image} alt={`Painting ${index + 1}`} />
+            </div>
+          ))
+        ) : (
+          <p>No images available for paintings.</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Paintings;
