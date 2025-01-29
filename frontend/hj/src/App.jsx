026264d46e5f79b317484c240b1eccd6d2ecb1a7
Sdg877 <sdrakegill@gmail.com>
2025-01-29 18:14:02 +0000

    Update to remove banner from homepage

--- frontend/hj/src/App.jsx
+++ frontend/hj/src/App.jsx
@@ -1,107 +1,3 @@
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   Navigate,
-// } from "react-router-dom";
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import Dashboard from "./admin/Dashboard.jsx";
-// import UploadImages from "./admin/components/ImageUpload.jsx";
-// import NewsUpdate from "./admin/components/NewsUpdate.jsx";
-// import AboutMe from "./site/pages/AboutMe.jsx";
-// import ContactMe from "./site/pages/ContactMe.jsx";
-// import Sculptures from "./site/components/sculptures.jsx";
-// import Paintings from "./site/components/paintings.jsx";
-// import Cards from "./site/components/cards.jsx";
-// import Textiles from "./site/components/textiles.jsx";
-// import Homepage from "./site/pages/Homepage.jsx";
-// import News from "./site/pages/News.jsx";
-// import Login from "./admin/components/Login.jsx";
-// import ImageThumbnail from "./admin/components/ImageThumbnail.jsx";
-// import PrivateRoute from "./PrivateRoute";
-// import Navbar from "../src/site/components/Navbar.jsx";
-// import DeleteNews from "./admin/components/DeleteNews.jsx";
-// import Layout from "./site/components/Layout.jsx";
-
-// function App() {
-//   return (
-//     <Layout>
-//     <Router>
-//       <div className="App">
-//         <Navbar />
-//         <Routes>
-//           <Route path="/login" element={<Login />} />
-
-//           <Route
-//             path="/admin"
-//             element={
-//               <PrivateRoute>
-//                 <Dashboard />
-//               </PrivateRoute>
-//             }
-//           />
-//           <Route
-//             path="/admin/images"
-//             element={
-//               <PrivateRoute>
-//                 <UploadImages />
-//               </PrivateRoute>
-//             }
-//           />
-//           <Route
-//             path="/admin/news"
-//             element={
-//               <PrivateRoute>
-//                 <NewsUpdate />
-//               </PrivateRoute>
-//             }
-//           />
-//           <Route
-//             path="/admin/thumnails"
-//             element={
-//               <PrivateRoute>
-//                 <ImageThumbnail />
-//               </PrivateRoute>
-//             }
-//           />
-//           <Route
-//             path="/admin/delete"
-//             element={
-//               <PrivateRoute>
-//                 <DeleteNews />
-//               </PrivateRoute>
-//             }
-//           />
-
-//           <Route path="/sculptures" element={<Sculptures />} />
-//           <Route path="/paintings" element={<Paintings />} />
-//           <Route path="/cards" element={<Cards />} />
-//           <Route path="/textiles" element={<Textiles />} />
-//           <Route path="/about" element={<AboutMe />} />
-//           <Route path="/news" element={<News />} />
-//           <Route path="/contact" element={<ContactMe />} />
-//           <Route path="/" element={<Homepage />} />
-
-//           <Route
-//             path="/dashboard/news/:newsId"
-//             element={
-//               <PrivateRoute>
-//                 <Dashboard />
-//               </PrivateRoute>
-//             }
-//           />
-
-//           <Route path="*" element={<Navigate to="/" replace />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//     </Layout>
-//   );
-// }
-
-// export default App;
 import {
   BrowserRouter as Router,
   Routes,
@@ -140,11 +36,10 @@ function App() {
 }
 
 function AppRoutes() {
-  const location = useLocation(); // Hook to get current route
+  const location = useLocation(); 
 
   return (
     <div className="App">
-      {/* Render Navbar only when the path is not '/' */}
       {location.pathname !== "/" && <Navbar />}
 
       <Routes>
@@ -170,6 +65,22 @@ function AppRoutes() {
           element={
             <PrivateRoute>
               <NewsUpdate />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/admin/thumbnails"
+          element={
+            <PrivateRoute>
+              <ImageThumbnail />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/admin/delete"
+          element={
+            <PrivateRoute>
+              <DeleteNews />
             </PrivateRoute>
           }
         />
