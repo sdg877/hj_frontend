64c19399ada9780a0884deeaaba5c8ec45428536
Sdg877 <sdrakegill@gmail.com>
2025-01-29 18:59:44 +0000

    Update to homepage styling

--- frontend/hj/src/site/pages/Homepage.jsx
+++ frontend/hj/src/site/pages/Homepage.jsx
@@ -1,12 +1,53 @@
-import React from 'react';
-import '../../App.css'
+import React from "react";
+import "../../App.css";
+import collageCards from "../../assets/collage-cards.jpg";
+import collagePaintings from "../../assets/collage-paintings.jpg";
+import collageSculpture from "/src/assets/collage-sculpture.jpg";
+import logo from "../../assets/logo.jpeg"; 
+import logoWriting from "../../assets/logo-writing.jpeg"; 
+import { Link } from "react-router-dom"; 
 
 const Homepage = () => {
   return (
-    <div className="homepage"> 
-      <div className="content-container"> 
-        {/* Your other content here */}
+    <div className="homepage-container">
+      <div className="logo-container">
+        <img
+          src={logoWriting}
+          alt="Logo Writing"
+          className="homepage-logo-writing"
+        />
+        <img src={logo} alt="Logo" className="homepage-logo" />
       </div>
+
+      {/* Image Gallery */}
+      <div className="image-gallery-homepage">
+        <div className="image-item">
+          <img
+            src={collageCards}
+            alt="Collage Cards"
+            className="homepage-image"
+          />
+        </div>
+        <div className="image-item">
+          <img
+            src={collagePaintings}
+            alt="Collage Paintings"
+            className="homepage-image"
+          />
+        </div>
+        <div className="image-item">
+          <img
+            src={collageSculpture}
+            alt="Collage Sculpture"
+            className="homepage-image"
+          />
+        </div>
+      </div>
+
+      {/* Arrow Navigation */}
+      <Link to="/about" className="about-link">
+        <span className="arrow">&#8595;</span> {/* Arrow for navigation */}
+      </Link>
     </div>
   );
 };
