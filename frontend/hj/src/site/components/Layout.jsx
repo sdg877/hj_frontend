3af9f9df84e753c7aa802bcb257db07e7fe76653
Sdg877 <sdrakegill@gmail.com>
2025-01-30 08:41:51 +0000

    Positioning banner underneath navbar

--- frontend/hj/src/site/components/Layout.jsx
+++ frontend/hj/src/site/components/Layout.jsx
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import "../../App.css";
-
 import banner from "../../assets/banner.jpg";
+import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
   const location = useLocation();
+  const showContent = location.pathname !== "/" && location.pathname !== "/login";
+  const navbarRef = useRef(null); 
 
-  const showBanner = location.pathname !== "/";
+  useEffect(() => {
+
+    if (navbarRef.current) {
+      const navbarHeight = navbarRef.current.offsetHeight;
+      document.documentElement.style.setProperty('--navbar-height', `${navbarHeight}px`);
+    }
+  }, []); 
+
+
   return (
-    <div>
-      {showBanner && (
-        <div className="banner">
-          <img src={banner} alt="Banner" className="banner-image" />
-        </div>
+    <div className="layout-container">
+      {showContent && (
+        <>
+          <Navbar ref={navbarRef} /> 
+          <div className="banner">
+            <img src={banner} alt="Banner" className="banner-image" />
+          </div>
+        </>
       )}
-
-      <nav className="navbar"></nav>
-
-      <div className="content">{children}</div>
+      <main className="content">
+        {children}
+      </main>
     </div>
   );
 };
