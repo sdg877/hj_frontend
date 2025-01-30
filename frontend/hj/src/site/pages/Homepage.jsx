a4b651b1b3383ea1a4d4394a68520c43ec5de7e6
Sdg877 <sdrakegill@gmail.com>
2025-01-30 09:28:11 +0000

    Links added to images

--- frontend/hj/src/site/pages/Homepage.jsx
+++ frontend/hj/src/site/pages/Homepage.jsx
@@ -3,9 +3,9 @@ import "../../App.css";
 import collageCards from "../../assets/collage-cards.jpg";
 import collagePaintings from "../../assets/collage-paintings.jpg";
 import collageSculpture from "/src/assets/collage-sculpture.jpg";
-import logo from "../../assets/logo.jpeg"; 
-import logoWriting from "../../assets/logo-writing.jpeg"; 
-import { Link } from "react-router-dom"; 
+import logo from "../../assets/logo.jpeg";
+import logoWriting from "../../assets/logo-writing.jpeg";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
@@ -22,31 +22,36 @@ const Homepage = () => {
       {/* Image Gallery */}
       <div className="image-gallery-homepage">
         <div className="image-item">
-          <img
-            src={collageCards}
-            alt="Collage Cards"
-            className="homepage-image"
-          />
+          <Link to="/cards">
+            <img
+              src={collageCards}
+              alt="Collage Cards"
+              className="homepage-image"
+            />
+          </Link>
         </div>
         <div className="image-item">
-          <img
-            src={collagePaintings}
-            alt="Collage Paintings"
-            className="homepage-image"
-          />
+          <Link to="/paintings">
+            <img
+              src={collagePaintings}
+              alt="Collage Paintings"
+              className="homepage-image"
+            />
+          </Link>
         </div>
         <div className="image-item">
-          <img
-            src={collageSculpture}
-            alt="Collage Sculpture"
-            className="homepage-image"
-          />
+          <Link to="/sculptures">
+            <img
+              src={collageSculpture}
+              alt="Collage Sculpture"
+              className="homepage-image"
+            />
+          </Link>
         </div>
       </div>
 
-      {/* Arrow Navigation */}
       <Link to="/about" className="about-link">
-        <span className="arrow">&#8595;</span> {/* Arrow for navigation */}
+        <span className="arrow">&#8595;</span>
       </Link>
     </div>
   );
