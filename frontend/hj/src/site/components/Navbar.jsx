0a9d9f583222e0b57d21ccee22f14717f409a5f3
Sdg877 <sdrakegill@gmail.com>
2025-02-06 05:43:44 +0000

    Removing logo and logo image

--- frontend/hj/src/site/components/Navbar.jsx
+++ frontend/hj/src/site/components/Navbar.jsx
@@ -1,53 +1,5 @@
-// NAVBAR with original logo
-// 
-// import React from "react";
-// import { Link, useLocation } from "react-router-dom";
-// import logoWriting from "../../assets/logo-writing.jpeg";
-// import logo from "../../assets/logo.jpeg";
-
-// const Navbar = () => {
-//   const location = useLocation();
-
-//   if (location.pathname.startsWith("/login")) {
-//     return null;
-//   }
-
-//   const links = [
-//     { to: "/paintings", label: "Paintings" },
-//     { to: "/sculptures", label: "Sculptures" },
-//     { to: "/cards", label: "Cards & Prints" },
-//     { to: "/news", label: "News" },
-//     { to: "/about", label: "About" },
-//     { to: "/contact", label: "Contact" },
-//   ];
-
-//   return (
-//     <nav className="navbar">
-//       <ul className="navbar-list">
-//         {links.map(({ to, label }) =>
-//           location.pathname !== to ? (
-//             <li key={to} className="navbar-item">
-//               <Link to={to} className="navbar-link">
-//                 {label}
-//               </Link>
-//             </li>
-//           ) : null
-//         )}
-//       </ul>
-//       <div className="logo-container">
-//         <img src={logoWriting} alt="Logo Writing" className="logo-writing" />
-//         <img src={logo} alt="Logo" className="logo" />
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
-
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import logo from "../../assets/logo.jpeg";
 
 const Navbar = () => {
   const location = useLocation();
@@ -92,7 +44,6 @@ const Navbar = () => {
       </ul>
       <div className="logo-container">
         <div className="logo-writing">htreharnejones</div>
-        <img src={logo} alt="Logo" className="logo" />
       </div>
     </nav>
   );
