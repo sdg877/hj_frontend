d7e57803e0d6f87c2d2bb61bb0b1280e74e760c2
Sdg877 <sdrakegill@gmail.com>
2025-02-11 07:41:04 +0000

    Update to include both versions

--- frontend/hj/src/site/pages/AboutMe.jsx
+++ frontend/hj/src/site/pages/AboutMe.jsx
@@ -1,67 +1,144 @@
+// ORIGINAL VERSION
+// import React from "react";
+// import cardImage from "../../assets/card.jpg";
+// import drawingImage from "../../assets/drawing.jpg";
+// import heatherStudio from "../../assets/heather-studio.jpeg";
+// import "../../App.css";
+
+// const AboutMe = () => {
+//   return (
+//     <div className="about-me-container">
+//       <div className="about-me-header">
+//         <h2 className="about-me-heading">About Heather Treharne Jones</h2>
+//       </div>
+
+//       <div className="about-me-content">
+//         <div className="text-image-container">
+//           <p className="about-me-text">
+//             Growing up in South Wales and Tanzania has instilled a love of the
+//             natural environment, seascapes and colour. Seascapes of South Wales
+//             are currently my source of inspiration. It can be the view from a
+//             cliff edge which gives a bird’s eye view of the beach below. Dawn
+//             light in a cloudless sky illuminating a cliff line. Wood and
+//             detritus discovered along the beach after a storm, asking to be
+//             picked up. Large rock formations and slabs, photographed, sketched
+//             and painted.
+//           </p>
+//           <div className="image-container right">
+//             <img
+//               src={heatherStudio}
+//               alt="Heather in her Studio"
+//               className="about-main-image"
+//             />
+//             <p className="image-caption">Heather in her Studio (2025)</p>
+//           </div>
+//         </div>
+
+//         <div className="text-image-container reverse">
+//           <div className="image-container left">
+//             <img src={cardImage} alt="Card" className="about-me-image" />
+//           </div>
+//           <p className="about-me-text">
+//             Living in London, I studied Art History at Tate Britain and
+//             Sculpture at Camden Arts Centre. I moved to Australia and studied
+//             for a BA Fine Art Sculpture at University of New South Wales,
+//             Sydney, 1995 – 1997. Participated in ‘Sculpture by the Sea’ Sydney
+//             1997. I was appointed inaugural Artist in Residence at Mt Tomah
+//             Botanic Garden NSW in 2006 – 2007 Exhibited ‘Eucalypti 103’ –
+//             botanical sculptures of gumnuts created in response to World
+//             Heritage listing given to the region in 2000. And ‘Where Lightning
+//             Strikes’ a sculptural installation at seven sites across the Garden.
+//           </p>
+//         </div>
+
+//         <div className="text-image-container">
+//           <p className="about-me-text">
+//             And returning to south Wales – key exhibitions St Donat’s Art Centre
+//             2013 ‘The Glamorgan Heritage Coast’ Llanover Hall ‘Open Exhibition’
+//             September 2024 Cardiff MADE ‘Winter Arts and Craft’ December 2024 I
+//             currently run an art group in Penarth which has been active for five
+//             years, teaching mixed media and art history. Previously taught
+//             sculpture, drawing, art history and watercolours in Adult Education.
+//           </p>
+//           <div className="image-container right">
+//             <img src={drawingImage} alt="Drawing" className="about-me-image" />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AboutMe;
+
 import React from "react";
 import cardImage from "../../assets/card.jpg";
 import drawingImage from "../../assets/drawing.jpg";
-import paintingImage from "../../assets/painting.jpg";
+import heatherStudio from "../../assets/heather-studio.jpeg";
 import "../../App.css";
 
 const AboutMe = () => {
   return (
     <div className="about-me-container">
-      <h2 className="about-me-heading">
-        About The Artist - Heather Treharne Jones
-      </h2>
+      <div className="about-me-header">
+        <h2 className="about-me-heading">About Heather Treharne Jones</h2>
+      </div>
 
       <div className="about-me-content">
         <div className="text-image-container">
           <p className="about-me-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Curabitur vehicula, metus ac fringilla sodales, velit arcu
-            tincidunt neque, vel posuere tortor orci id nunc. Integer nec metus
-            at dolor varius tincidunt nec vel libero. Fusce tincidunt
-            scelerisque justo, in tristique magna hendrerit nec.
+            Growing up in South Wales and Tanzania gave me a deep appreciation
+            for the natural world—its colours, textures, and shifting
+            landscapes. Today, the seascapes of South Wales continue to inspire
+            my work. Whether it's the view from a cliff edge, the soft light of
+            dawn on rugged coastlines, or the weathered driftwood found after a
+            storm, I am drawn to capturing nature’s raw beauty.
           </p>
           <div className="image-container right">
-            <img src={cardImage} alt="Card" className="about-me-image" />
+            <img
+              src={heatherStudio}
+              alt="Heather in her Studio"
+              className="about-main-image"
+            />
+            <p className="image-caption">Heather in her Studio (2025)</p>
           </div>
         </div>
 
         <div className="text-image-container reverse">
           <div className="image-container left">
-            <img src={drawingImage} alt="Drawing" className="about-me-image" />
+            <img src={cardImage} alt="Card" className="about-me-image" />
           </div>
           <p className="about-me-text">
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-            officia deserunt mollit anim id est laborum. Neque porro quisquam
-            est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-            velit, sed quia non numquam eius modi tempora incidunt ut labore et
-            dolore magnam aliquam quaerat voluptatem. Phasellus interdum justo
-            eget est scelerisque, a aliquet mauris feugiat. Nam euismod, arcu id
-            viverra aliquet, risus purus maximus magna, id dictum nunc velit non
-            nunc. Duis non eros eu odio consequat consequat.
+            I studied Art History at Tate Britain and Sculpture at Camden Arts
+            Centre before moving to Australia, where I earned a BA in Fine Art
+            Sculpture from the University of New South Wales. During my time in
+            Sydney, I participated in ‘Sculpture by the Sea’ (1997), an
+            experience that shaped my approach to large-scale outdoor
+            installations. Later, as the inaugural Artist in Residence at Mt
+            Tomah Botanic Garden (2006–2007), I explored botanical themes,
+            creating sculptural works such as Eucalypti 103 and Where Lightning
+            Strikes, a multi-site installation inspired by the region’s World
+            Heritage listing.
           </p>
         </div>
 
         <div className="text-image-container">
           <p className="about-me-text">
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui
-            blanditiis praesentium voluptatum deleniti atque corrupti quos
-            dolores et quas molestias excepturi sint occaecati cupiditate non
-            provident, similique sunt in culpa qui officia deserunt mollitia
-            animi, id est laborum et dolorum fuga. Mauris non eros a nulla
-            cursus mattis. Vestibulum non lectus ut justo porttitor tincidunt.
-            Suspendisse potenti. Etiam posuere, nulla vel tempor elementum, elit
-            sem euismod justo, at scelerisque sapien lectus vel purus.
+            Since returning to South Wales, my work has been exhibited at: 
+            <br />
+            St Donat’s Art Centre (2013): The Glamorgan Heritage Coast 
+            <br />
+            Llanover Hall (September 2024): Open Exhibition 
+            <br />
+            Cardiff MADE (December 2024): Winter Arts and Craft 
+            <br /><br />
+            Alongside my practice, I run a mixed-media and
+            art history group in Penarth, which has been active for five years.
+            My teaching experience includes sculpture, drawing, art history, and
+            watercolours in adult education.
           </p>
           <div className="image-container right">
-            <img
-              src={paintingImage}
-              alt="Painting"
-              className="about-me-image"
-            />
+            <img src={drawingImage} alt="Drawing" className="about-me-image" />
           </div>
         </div>
       </div>
