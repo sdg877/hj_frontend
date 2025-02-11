a31c265fee5c13c85052efb4d7944ce324053c1a
Sdg877 <sdrakegill@gmail.com>
2025-02-11 08:04:06 +0000

    Update to component to display spinner correctly

--- frontend/hj/src/admin/components/DeleteImages.jsx
+++ frontend/hj/src/admin/components/DeleteImages.jsx
@@ -3,14 +3,20 @@ import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Spinner from "../../site/components/Spinner";
 
 const DeleteImages = () => {
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [imagesLoaded, setImagesLoaded] = useState(0);
+  const [loadingMessage, setLoadingMessage] = useState(
+    "Please wait while your images are loading..."
+  );
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchImages = async () => {
+      setLoading(true);
       try {
         const response = await axios.get(
           `${import.meta.env.VITE_BACKEND_URL}/admin/thumbnails`
@@ -27,8 +33,7 @@ const DeleteImages = () => {
     fetchImages();
   }, []);
 
-  const handleDelete = async (imageUrl) => { 
-
+  const handleDelete = async (imageUrl) => {
     const token = localStorage.getItem("token");
     if (token) {
       try {
@@ -45,32 +50,34 @@ const DeleteImages = () => {
           headers: {
             Authorization: `Bearer ${token}`,
           },
-          data: { imageUrl }, 
+          data: { imageUrl },
         }
       );
 
       setImages((prevImages) =>
-        prevImages.filter((img) => img.url !== imageUrl) // Filter by URL
+        prevImages.filter((img) => img.url !== imageUrl)
       );
-
       toast.success(response.data.message);
     } catch (err) {
       console.error("Error deleting image:", err);
       toast.error("Failed to delete image");
-
-      if (err.response) {
-        console.error("Response data:", err.response.data);
-        console.error("Response status:", err.response.status);
-        console.error("Response headers:", err.response.headers);
-      } else if (err.request) {
-        console.error("Request:", err.request);
-      } else {
-        console.error("Error message:", err.message);
-      }
     }
   };
 
-  if (loading) return <p>Loading images...</p>;
+  const handleImageLoad = () => {
+    setImagesLoaded((prevCount) => prevCount + 1);
+  };
+
+  if (loading) {
+    return (
+      <div className="spinner-container">
+        <div className="spinner-wrapper">
+          <Spinner />
+        </div>
+      </div>
+    );
+  }
+
   if (error) return <p>{error}</p>;
 
   return (
@@ -82,16 +89,47 @@ const DeleteImages = () => {
         ) : (
           images.map((image, index) => (
             <div key={index} className="image-item">
-              <img src={image.url} alt={`Image ${index}`} className="image-thumbnail" onError={(e) => console.error("Image load error:", e)} />
+              <div className="image-thumbnail-container">
+                <img
+                  src={image.url}
+                  alt={`Image ${index}`}
+                  className="image-thumbnail"
+                  loading="lazy"
+                  onError={(e) => console.error("Image load error:", e)}
+                  onLoad={handleImageLoad}
+                />
+              </div>
               <p className="image-category">{image.category}</p>
-              <button onClick={() => handleDelete(image.url)} className="delete-button"> 
+              <button
+                onClick={() => handleDelete(image.url)}
+                className="delete-button"
+              >
                 X
               </button>
             </div>
           ))
         )}
       </div>
-      <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+
+      {imagesLoaded < images.length && (
+        <div>
+          <div>
+            <Spinner />
+          </div>
+        </div>
+      )}
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 };
