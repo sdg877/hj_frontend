56709b982138e1d120307fc84b0472780ba8971d
Sdg877 <sdrakegill@gmail.com>
2025-02-12 10:42:45 +0000

    Update to dashboard content

--- frontend/hj/src/admin/Dashboard.jsx
+++ frontend/hj/src/admin/Dashboard.jsx
@@ -1,33 +1,3 @@
-// import React from "react";
-// import { useNavigate } from "react-router-dom";
-
-// const Dashboard = () => {
-//   const navigate = useNavigate();
-
-//   return (
-//     <div>
-//       <h2 className="dashboard-title">Hi Heather. <br/><br/>Welcome To Your Dashboard.
-//       </h2>
-//       <div className="dashboard-container">
-//         <button
-//           className="dashboard-button"
-//           onClick={() => navigate("/admin/images")}
-//         >
-//           Images
-//         </button>
-//         <button
-//           className="dashboard-button"
-//           onClick={() => navigate("/admin/news")}
-//         >
-//           News
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Dashboard;
-
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
