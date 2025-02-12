bd3e00e32033bc694c59f03eaeb83524b8a7f775
Sdg877 <sdrakegill@gmail.com>
2025-02-12 10:42:18 +0000

    Update to dashboard content

--- frontend/hj/src/admin/Dashboard.jsx
+++ frontend/hj/src/admin/Dashboard.jsx
@@ -1,3 +1,33 @@
+// import React from "react";
+// import { useNavigate } from "react-router-dom";
+
+// const Dashboard = () => {
+//   const navigate = useNavigate();
+
+//   return (
+//     <div>
+//       <h2 className="dashboard-title">Hi Heather. <br/><br/>Welcome To Your Dashboard.
+//       </h2>
+//       <div className="dashboard-container">
+//         <button
+//           className="dashboard-button"
+//           onClick={() => navigate("/admin/images")}
+//         >
+//           Images
+//         </button>
+//         <button
+//           className="dashboard-button"
+//           onClick={() => navigate("/admin/news")}
+//         >
+//           News
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Dashboard;
+
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -6,20 +36,22 @@ const Dashboard = () => {
 
   return (
     <div>
-      <h2 className="dashboard-title">Hi Heather. Welcome To Your Dashboard.
+      <h2 className="dashboard-title">
+        Welcome back, Heather! <br /><br />
+        This is your exclusive admin dashboard.
       </h2>
       <div className="dashboard-container">
         <button
           className="dashboard-button"
           onClick={() => navigate("/admin/images")}
         >
-          Images
+          Manage Images
         </button>
         <button
           className="dashboard-button"
           onClick={() => navigate("/admin/news")}
         >
-          News
+          Manage News
         </button>
       </div>
     </div>
