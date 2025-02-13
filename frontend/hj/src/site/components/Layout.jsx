ef7c559d98703773385187a046fca91fcb59bec3
Sdg877 <sdrakegill@gmail.com>
2025-02-13 06:47:01 +0000

    Banner image moved to Navbar component

--- frontend/hj/src/site/components/Layout.jsx
+++ frontend/hj/src/site/components/Layout.jsx
@@ -1,36 +1,16 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import { useLocation } from "react-router-dom";
 import "../../App.css";
-import banner from "../../assets/banner.jpg";
 import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
   const location = useLocation();
   const showContent = location.pathname !== "/login";
-  const navbarRef = useRef(null); 
-
-  useEffect(() => {
-
-    if (navbarRef.current) {
-      const navbarHeight = navbarRef.current.offsetHeight;
-      document.documentElement.style.setProperty('--navbar-height', `${navbarHeight}px`);
-    }
-  }, []); 
-
 
   return (
     <div className="layout-container">
-      {showContent && (
-        <>
-          <Navbar ref={navbarRef} /> 
-          <div className="banner">
-            <img src={banner} alt="Banner" className="banner-image" />
-          </div>
-        </>
-      )}
-      <main className="content">
-        {children}
-      </main>
+      {showContent && <Navbar />}
+      <main className="content">{children}</main>
     </div>
   );
 };
