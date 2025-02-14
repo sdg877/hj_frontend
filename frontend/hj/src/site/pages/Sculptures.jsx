155032900d0f4f3d395cee0c7b82eda6a5efc58f
Sdg877 <sdrakegill@gmail.com>
2025-02-14 06:02:41 +0000

    Adding sculpture text

--- frontend/hj/src/site/pages/Sculptures.jsx
+++ frontend/hj/src/site/pages/Sculptures.jsx
@@ -1,8 +1,18 @@
-import React from "react";
-import Gallery from "./Gallery";
+import Gallery from './Gallery';
+import React from 'react';
 
 const Sculptures = () => {
-  return <Gallery endpoint="sculptures" title="Sculptures" />;
+  const sculpturesDescription = [
+    "For these sculptures, I’ve carved alabaster and limestone, assembled found objects from the shoreline, and sculpted in clay. While these sculptures are not for sale, I do accept commissions for sculptural works. Please get in touch if you would like a bespoke artwork."
+  ];
+
+  return (
+    <Gallery 
+      endpoint="sculptures"  
+      title="Sculptures"   
+      description={sculpturesDescription}
+    />
+  );
 };
 
 export default Sculptures;
