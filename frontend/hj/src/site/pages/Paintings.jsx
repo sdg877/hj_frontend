9fa64419af94cb77b0e2b1c80b9a3ab6fbb34d79
Sdg877 <sdrakegill@gmail.com>
2025-02-14 06:17:35 +0000

    text for paintings added

--- frontend/hj/src/site/pages/Paintings.jsx
+++ frontend/hj/src/site/pages/Paintings.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import Gallery from "./Gallery";
+import { Link } from 'react-router-dom';
 
 const Paintings = () => {
-  return <Gallery endpoint="paintings" title="Paintings" />;
+  const paintingsDescription = [
+    "The Treelines series is mixed media; using collage with watercolours to add definition to forms. The Coastal series is painted in acrylics.",
+    "All paintings are available as Giclee prints using a high quality art paper, G.F. Smith Colourplan in ‘natural’ 270g.",
+    <span>Please <Link to="/contact" className="contact-link">get in touch</Link> for more information and orders.</span>
+  ];
+
+  return (
+    <Gallery 
+      endpoint="paintings" 
+      title="Paintings" 
+      description={paintingsDescription} 
+    />
+  );
 };
 
 export default Paintings;
