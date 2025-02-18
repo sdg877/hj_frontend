e4386ae080f8f2e417c07118b75980a33abc835d
Sdg877 <sdrakegill@gmail.com>
2025-02-18 07:10:11 +0000

    Footer added

--- frontend/hj/src/site/components/Layout.jsx
+++ frontend/hj/src/site/components/Layout.jsx
@@ -1,3 +1,31 @@
+// import React from "react";
+// import { useLocation } from "react-router-dom";
+// import "../../App.css";
+// import Navbar from "./Navbar";
+// import banner from "../../assets/banner.jpg";
+
+// const Layout = ({ children }) => {
+//   const location = useLocation();
+//   const showContent = location.pathname !== "/login";
+//   const showBanner = location.pathname !== "/" && location.pathname !== "/login";
+
+//   return (
+//     <div className="layout-container">
+//       {showBanner && (
+//         <div className="banner">
+//           <img src={banner} alt="Banner" className="banner-image" />
+//         </div>
+//       )}
+
+//       {showContent && <Navbar />}
+
+//       <main className="content">{children}</main>
+//     </div>
+//   );
+// };
+
+// export default Layout;
+
 import React from "react";
 import { useLocation } from "react-router-dom";
 import "../../App.css";
@@ -7,7 +35,8 @@ import banner from "../../assets/banner.jpg";
 const Layout = ({ children }) => {
   const location = useLocation();
   const showContent = location.pathname !== "/login";
-  const showBanner = location.pathname !== "/" && location.pathname !== "/login";
+  const showBanner =
+    location.pathname !== "/" && location.pathname !== "/login";
 
   return (
     <div className="layout-container">
@@ -20,6 +49,8 @@ const Layout = ({ children }) => {
       {showContent && <Navbar />}
 
       <main className="content">{children}</main>
+
+      <footer className="footer">© 2025 Heather Treharne Jones</footer>
     </div>
   );
 };
