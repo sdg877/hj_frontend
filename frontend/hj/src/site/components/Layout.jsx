c274ecd1a18f2819b76cef3fdf83ea0d10f29715
Sdg877 <sdrakegill@gmail.com>
2025-02-19 05:34:20 +0000

    Footer added

--- frontend/hj/src/site/components/Layout.jsx
+++ frontend/hj/src/site/components/Layout.jsx
@@ -1,31 +1,3 @@
-// import React from "react";
-// import { useLocation } from "react-router-dom";
-// import "../../App.css";
-// import Navbar from "./Navbar";
-// import banner from "../../assets/banner.jpg";
-
-// const Layout = ({ children }) => {
-//   const location = useLocation();
-//   const showContent = location.pathname !== "/login";
-//   const showBanner = location.pathname !== "/" && location.pathname !== "/login";
-
-//   return (
-//     <div className="layout-container">
-//       {showBanner && (
-//         <div className="banner">
-//           <img src={banner} alt="Banner" className="banner-image" />
-//         </div>
-//       )}
-
-//       {showContent && <Navbar />}
-
-//       <main className="content">{children}</main>
-//     </div>
-//   );
-// };
-
-// export default Layout;
-
 import React from "react";
 import { useLocation } from "react-router-dom";
 import "../../App.css";
